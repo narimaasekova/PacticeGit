460c2c87f61e6e675ea131e5808781ff70273333
narima asekova <nari_100591@mail.ru>
2018-07-08 14:33:14 -0700

    narima

---
+++ src/test/java/com/cybertek/Batch8.java
@@ -0,0 +1,11 @@
+package com.cybertek;
+
+import com.github.javafaker.Faker;
+
+public class Batch8 {
+public static void main(String[] args) {
+	System.out.println("AAshn");
+	Faker faker =new Faker();
+	
+}
+}
