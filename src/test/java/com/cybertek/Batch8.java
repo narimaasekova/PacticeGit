278ea2b2a4c91d13dd05de311b1b792fd16e0dd0
CybertekSchool <github@cybertekschool.coom>
2018-07-08 17:44:33 -0500

    dsa

--- src/test/java/com/cybertek/Batch8.java
+++ src/test/java/com/cybertek/Batch8.java
@@ -3,10 +3,10 @@ package com.cybertek;
 import com.github.javafaker.Faker;
 
 public class Batch8 {
-public static void main(String[] args) {
-	System.out.println("homePage branch");
-	Faker faker =new Faker();
+	public static void main(String[] args) {
+		System.out.println("homePage branch");
+		Faker faker =new Faker();
 
-	System.out.println("Narima");
-}
+		System.out.println("Sinan");
+	}
 }
